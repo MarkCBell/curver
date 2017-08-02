8b2e76133b7077f2b4a5f234f569d4ef0d405a82
Mark Bell <mcbell@illinois.edu>
2017-08-03 00:25:33 +0100

    Added memoize.

--- source/kernel/utilities.py
+++ source/kernel/utilities.py
@@ -3,6 +3,7 @@
 
 import curver
 
+from functools import partial
 from itertools import product, groupby
 from string import ascii_lowercase
 
@@ -65,4 +66,34 @@ class UnionFind(object):
 	def union(self, *arg):
 		for item in arg:
 			self.union2(arg[0], item)
+
+class memoize(object):
+	''' Cache the return value of a method.
+	
+	This class is meant to be used as a decorator of methods. The return value
+	from a given method invocation will be cached on the instance whose method
+	was invoked. All arguments passed to a method decorated with memoize must
+	be hashable.
+	
+	If a memoized method is invoked directly on its class the result will not
+	be cached. Instead the method will be invoked like a static method. '''
+	
+	def __init__(self, func):
+		self.func = func
+	def __get__(self, obj, objtype=None):
+		if obj is None:
+			return self.func
+		return partial(self, obj)
+	def __call__(self, *args, **kw):
+		obj = args[0]
+		try:
+			cache = obj.__cache
+		except AttributeError:
+			cache = obj.__cache = {}
+		key = (self.func, args[1:], frozenset(kw.items()))
+		try:
+			res = cache[key]
+		except KeyError:
+			res = cache[key] = self.func(*args, **kw)
+		return res
 
