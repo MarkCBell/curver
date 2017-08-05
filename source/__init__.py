977895edc00f3054ff7487640ed7286eff9b6640
Mark Bell <mcbell@illinois.edu>
2017-08-06 00:45:31 +0100

    Catch warnings.

--- source/__init__.py
+++ source/__init__.py
@@ -1,12 +1,16 @@
 
 ''' Curver is a program for computations in the curve complex. '''
 
-import pkg_resources
 import curver.kernel
 from curver.load import load
 from numbers import Integral as IntegerType
 
-__version__ = pkg_resources.require('curver')[0].version
+import warnings
+
+with warnings.catch_warnings():
+	warnings.simplefilter("ignore")
+	import pkg_resources  # Suppress 'UserWarning: Module curver was already imported from ...'
+	__version__ = pkg_resources.require('curver')[0].version
 
 # Set up really short names for the most commonly used classes and functions by users.
 create_triangulation = curver.kernel.create_triangulation
