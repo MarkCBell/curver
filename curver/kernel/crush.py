e75a6e19ec1209b1fcdd50e227839cf10ef8092e
Mark Bell <mcbell@illinois.edu>
2017-08-09 19:50:45 +0100

    Completed crush in non-isolating case.

--- curver/kernel/crush.py
+++ curver/kernel/crush.py
@@ -20,20 +20,23 @@ class Crush(Move):
 	
 	def apply_lamination(self, lamination):
 		
+		geometric = list(lamination)
 		if self.curve.weight() == 2:
-			e1, e2 = [index for index in self.source_triangulation.indices if self.curve(index) > 0]
-			# We might need to swap these edge indices so we have a good frame of reference.
-			if self.source_triangulation.corner_lookup[e1].indices[2] != e2: e1, e2 = e2, e1
+			# Get some edges.
+			a = self.curve.parallel()
+			_, b, e = self.source_triangulation.corner_lookup[a.label]
+			_, c, d = self.source_triangulation.corner_lookup[~e.label]
 			
-			a, b, c, d, e = self.source_triangulation.square(e1)
-			u, v, w, x, y, z = lamination.dual_square(e)
-			
-			geometric = list(lamination)
+			# some edge_weight < 0 ==> some side_weight <= 0.
 			if lamination(b) < 0 or lamination(e) < 0:
 				new_b = new_e = lamination(b) + lamination(e)
 			else:
-				new_b = min(v, w, y)
-				new_e = min(u, x, z)
+				u = self.source_triangulation.vertex_lookup[a.label]  # = self.triangulation.vertex_lookup[~a.label].
+				new_b = min(lamination.side_weight(edge) for edge in curver.kernel.utilities.cyclic_slice(u, a, ~a))  # The set of edges that come out of v from a round to ~a.
+				
+				v = self.source_triangulation.vertex_lookup[c.label]  # = self.triangulation.vertex_lookup[~c.label].
+				new_e = min(lamination.side_weight(edge) for edge in curver.kernel.utilities.cyclic_slice(v, c, ~c))
+			
 			geometric[b.index] = new_b
 			geometric[e.index] = new_e
 		else:
