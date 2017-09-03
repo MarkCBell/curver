2248072609f83c8355a35c59405c90144ac2f631
Mark Bell <mcbell@illinois.edu>
2017-09-03 15:08:01 +0100

    Using new is_isolating.

--- curver/kernel/crush.py
+++ curver/kernel/crush.py
@@ -25,25 +25,7 @@ class Crush(Move):
 	
 	def apply_lamination(self, lamination):
 		geometric = list(lamination)
-		if self.curve.weight() == 2:  # Non-isolating.
-			# Get some edges.
-			a = self.curve.parallel()
-			_, b, e = self.source_triangulation.corner_lookup[a.label]
-			_, c, d = self.source_triangulation.corner_lookup[~e.label]
-			
-			u = self.source_triangulation.vertex_lookup[a.label]  # = self.triangulation.vertex_lookup[~a.label].
-			u_edges = curver.kernel.utilities.cyclic_slice(u, a, ~a)
-			around_u = min(max(lamination.side_weight(edge), 0) for edge in u_edges)  # The number of components that go through a, around u and then back through ~a.
-			out_u = sum(max(-lamination.side_weight(edge), 0) for edge in u_edges) + sum(max(-lamination(edge), 0) for edge in u_edges[1:])  # The number of arcs that come out of u (from a around to ~a).
-			# Since around_u > 0 ==> out_u == 0 & out_u > 0 ==> around_u == 0, this is equivalent to around_u if around_u > 0 else -out_u
-			geometric[b.index] = around_u - out_u
-			
-			v = self.source_triangulation.vertex_lookup[c.label]  # = self.triangulation.vertex_lookup[~c.label].
-			v_edges = curver.kernel.utilities.cyclic_slice(v, c, ~c)
-			around_v = min(max(lamination.side_weight(edge), 0) for edge in v_edges)
-			out_v = sum(max(-lamination.side_weight(edge), 0) for edge in v_edges) + sum(max(-lamination(edge), 0) for edge in v_edges[1:])
-			geometric[e.index] = around_v - out_v  # Same trick.
-		else:  # self.curve is isolating.
+		if self.curve.is_isolating():
 			# Get some edges.
 			a = self.curve.parallel()
 			v = self.curve.triangulation.vertex_lookup[a.label]  # = self.triangulation.vertex_lookup[~a.label].
@@ -67,6 +49,24 @@ class Crush(Move):
 			
 			# Now have to reset b weight.
 			geometric[b.index] = around_v - out_v  # Same trick as above.
+		else:  # self.curve is non-isolating.
+			# Get some edges.
+			a = self.curve.parallel()
+			_, b, e = self.source_triangulation.corner_lookup[a.label]
+			_, c, d = self.source_triangulation.corner_lookup[~e.label]
+			
+			u = self.source_triangulation.vertex_lookup[a.label]  # = self.triangulation.vertex_lookup[~a.label].
+			u_edges = curver.kernel.utilities.cyclic_slice(u, a, ~a)
+			around_u = min(max(lamination.side_weight(edge), 0) for edge in u_edges)  # The number of components that go through a, around u and then back through ~a.
+			out_u = sum(max(-lamination.side_weight(edge), 0) for edge in u_edges) + sum(max(-lamination(edge), 0) for edge in u_edges[1:])  # The number of arcs that come out of u (from a around to ~a).
+			# Since around_u > 0 ==> out_u == 0 & out_u > 0 ==> around_u == 0, this is equivalent to around_u if around_u > 0 else -out_u
+			geometric[b.index] = around_u - out_u
+			
+			v = self.source_triangulation.vertex_lookup[c.label]  # = self.triangulation.vertex_lookup[~c.label].
+			v_edges = curver.kernel.utilities.cyclic_slice(v, c, ~c)
+			around_v = min(max(lamination.side_weight(edge), 0) for edge in v_edges)
+			out_v = sum(max(-lamination.side_weight(edge), 0) for edge in v_edges) + sum(max(-lamination(edge), 0) for edge in v_edges[1:])
+			geometric[e.index] = around_v - out_v  # Same trick.
 		
 		return self.target_triangulation.lamination(geometric)  # Have to promote.
 	
