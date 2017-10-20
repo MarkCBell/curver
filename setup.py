9496ce68923734f5857a9083607f5458fd5ddad1
Mark Bell <mcbell@illinois.edu>
2017-10-21 00:38:23 +0100

    Added long_description and classifiers.

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 
+import os
 try:
 	from setuptools import setup
 except ImportError:
 	print('Unable to import setuptools, using distutils instead.')
 	from distutils.core import setup
 
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
 	name='curver',
 	version='0.1.0',
 	description='For calculations in the curve complex',
+	long_description=read('README'),
 	author='Mark Bell',
 	author_email='mcbell@illinois.edu',
 	url='https://bitbucket.org/Mark_Bell/curver',
@@ -25,6 +30,16 @@ setup(
 	install_requires=[
 		'hypothesis',  # tests.hyp
 		'networkx',  # Curve.tight_geodesic
-		]
+		],
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Education',
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Scientific/Engineering :: Mathematics',
+		],
 	)
 
