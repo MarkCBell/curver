ee9c26f5033a59ab891140f08ffef8fd6f4f98fc
Mark Bell <mcbell@illinois.edu>
2017-11-28 10:57:05 +0000

    Fixed DOS line endings.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='curver',
-    version='0.1.0',
+    version='0.0.1',
     description='For calculations in the curve complex',
     long_description=read('README'),
     author='Mark Bell',
