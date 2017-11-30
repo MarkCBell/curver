3535957821e24b39a60b9c444bffeb4364e2fdaa
Mark Bell <mcbell@illinois.edu>
2017-11-30 11:25:05 +0000

    Better D constant.

--- curver/kernel/curvecomplex.py
+++ curver/kernel/curvecomplex.py
@@ -12,13 +12,17 @@ class CurveComplex(object):
     def __init__(self, triangulation):
         self.triangulation = triangulation
         self.zeta = self.triangulation.zeta
+        assert(self.triangulation.is_connected())
+        
         # Constants:
         # Universal:
         self.QUASICONVEXITY = 10  # Masur-Minsky.  # Ask Katie Vokes about this.
         self.BOUNDED_GEODESIC_IMAGE = 100  # BGIT.
         self.HYPERBOLICITY = 17  # Curve complex delta-hyperbolicity.
         # Uniform:
-        self.D = factorial(self.zeta**(2 * self.zeta * (2*self.HYPERBOLICITY + 2)**2))  # Bowditch bound on denominator [Bow08]. This explict bound comes from Theorems 6.2 & 6.3 of [Webb15] and Algorithm 3 of [BellWebb16].
+        self.D = (self.zeta**(2 * self.zeta * (2*self.HYPERBOLICITY + 2)**2))**2  # Bowditch bound on denominator [Bow08].
+        # This explict bound comes from Theorems 6.2 & 6.3 of [Webb15] and Algorithm 3 of [BellWebb16].
+        # TODO 1) Recheck this.
         self.XI = self.zeta // 2  # Actually should be 3g - 3 + n < 3g - 3 + 1.5n = zeta / 2
         self.M = 28 * factorial(self.XI) * self.HYPERBOLICITY * self.D * self.BOUNDED_GEODESIC_IMAGE
         E, n = self.triangulation.euler_characteristic, self.triangulation.num_vertices
