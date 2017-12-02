022ef1f6071104b7f827059fb2d471702a31d72a
Mark Bell <mcbell@illinois.edu>
2017-12-02 11:26:06 +0000

    Testing all braid relations.

--- tests/test_mappingclassgroup.py
+++ tests/test_mappingclassgroup.py
@@ -30,13 +30,17 @@ class TestMCG(unittest.TestCase):
         strn = pickle.dumps(mcg)
         self.assertEqual(mcg, pickle.loads(strn))
     
-    @given(braid_groups())
-    def test_braid_relation(self, braid_group):
-        p = braid_group.triangulation.num_vertices
-        for i in range(p):
-            self.assertEqual(braid_group('s_%ds_%ds_%d' % (i, (i+1) % p, i)), braid_group('s_%ds_%ds_%d' % ((i+1) % p, i, (i+1) % p)))
-    
-    @given(mcgs_genus())
+    @given(mcgs())
     def test_braid_relation(self, mcg):
-        pass
+        p = mcg.triangulation.num_vertices
+        g = (mcg.triangulation.euler_characteristic + p - 2) // 2
+        for i in range(p - 2):
+            self.assertEqual(mcg('s_%ds_%ds_%d' % (i, (i+1) % p, i)), mcg('s_%ds_%ds_%d' % ((i+1) % p, i, (i+1) % p)))
+        for i in range(g):
+            self.assertEqual(mcg('a_%db_%da_%d' % (i, i, i)), mcg('b_%da_%db_%d' % (i, i, i)))
+        for i in range(g-1):
+            self.assertEqual(mcg('b_%dc_%db_%d' % (i, i, i)), mcg('c_%db_%dc_%d' % (i, i, i)))
+        if g > 0:
+            for i in range(1, p):
+                self.assertEqual(mcg('p_%db_%dp_%d' % (i, g-1, i)), mcg('b_%dp_%db_%d' % (g-1, i, g-1)))
 
