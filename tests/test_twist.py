f24c42aa7f6cee6df4ef2272bbf82672cc539c60
Mark Bell <mcbell@illinois.edu>
2017-12-04 11:33:09 +0000

    Initial twist tests.

---
+++ tests/test_twist.py
@@ -0,0 +1,30 @@
+
+from hypothesis import given, settings
+import hypothesis.strategies as st
+import pickle
+import pytest
+import unittest
+
+import curver
+import strategies
+
+class TestTwist(unittest.TestCase):
+    @given(strategies.curves(), st.integers())
+    @settings(max_examples=1, deadline=None)
+    def test_pickle(self, curve, power):
+        twist_i = curve.encode_twist(power)
+        
+        self.assertEqual(twist_i, pickle.loads(pickle.dumps(twist_i)))
+    
+    @given(strategies.curves(), st.integers(), st.integers())
+    @settings(max_examples=1, deadline=None)
+    def test_powers(self, curve, power1, power2):
+        twist_i = curve.encode_twist(power1)
+        twist_j = curve.encode_twist(power2)
+        twist_ij = curve.encode_twist(power1 + power2)
+        twist_neg_i = curve.encode_twist(-power1)
+        
+        self.assertEqual(twist_i * twist_j, twist_j * twist_i)  # Commute.
+        self.assertEqual(twist_i * twist_j, twist_ij)  # Additive.
+        self.assertEqual(twist_neg_i, ~twist_i)  # Inverse.
+
