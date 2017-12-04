3306085e0d7077980ae4bb1b60aa67ab3254c6f0
Mark Bell <mcbell@illinois.edu>
2017-12-04 02:35:10 +0000

    Moved slow mark to individual tests.

--- tests/test_lamination.py
+++ tests/test_lamination.py
@@ -8,24 +8,27 @@ import unittest
 import curver
 import strategies
 
-@pytest.mark.slow
 class TestLamination(unittest.TestCase):
+    @pytest.mark.slow
     @given(strategies.laminations())
     def test_pickle(self, lamination):
         self.assertEqual(lamination, pickle.loads(pickle.dumps(lamination)))
     
+    @pytest.mark.slow
     @given(st.data())
     def test_hash(self, data):
         lamination1 = data.draw(strategies.laminations())
         lamination2 = data.draw(strategies.laminations(lamination1.triangulation))
         self.assertTrue(hash(lamination1) != hash(lamination2) or lamination1 == lamination2)
     
+    @pytest.mark.slow
     @given(st.data())
     def test_orientation(self, data):
         lamination = data.draw(strategies.laminations())
         edge = data.draw(st.sampled_from(lamination.triangulation.edges))
         self.assertEqual(lamination(edge), lamination(~edge))
 
+    @pytest.mark.slow
     @given(strategies.laminations())
     def test_components(self, lamination):
         self.assertEqual(lamination.triangulation.sum([multiplicity * component for component, multiplicity in lamination.components().items()]), lamination)
@@ -33,12 +36,14 @@ class TestLamination(unittest.TestCase):
         for component in lamination:
             self.assertEqual(component.intersection(component), 0)
     
+    @pytest.mark.slow
     @given(st.data())
     def test_components_image(self, data):
         lamination = data.draw(strategies.laminations())
         encoding = data.draw(strategies.encodings(lamination.triangulation))
         self.assertEqual(set(encoding(lamination).components()), {encoding(component) for component in lamination.components()})
     
+    @pytest.mark.slow
     @given(st.data())
     def test_intersection(self, data):
         lamination1 = data.draw(strategies.laminations())
