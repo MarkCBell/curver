21ef2c701fdc15726d23a0d0ad45026560faecf7
Mark Bell <mcbell@illinois.edu>
2017-12-07 15:29:09 +0000

    Using load_profile.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 
+import os
+from hypothesis import settings, Verbosity, HealthCheck
 import pytest
 
 def pytest_addoption(parser):
@@ -10,4 +12,10 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if 'slow' in item.keywords:
                 item.add_marker(skip_slow)
+
+
+settings.register_profile('ci', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow,)))
+settings.register_profile('dev', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow,), max_examples=5))
+settings.register_profile('debug', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow,), max_examples=10, verbosity=Verbosity.verbose))
+settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'default'))
 
