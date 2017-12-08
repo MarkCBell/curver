d8d2d6aaf227faabef549e4cbce57ac2f89baea4
Mark Bell <mcbell@illinois.edu>
2017-12-08 15:06:13 +0000

    Dev is now the default hypothesis profile.

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,5 +17,5 @@ def pytest_collection_modifyitems(config, items):
 settings.register_profile('ci', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow,)))
 settings.register_profile('dev', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow,), max_examples=5))
 settings.register_profile('debug', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow,), max_examples=5, verbosity=Verbosity.verbose))
-settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'default'))
+settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'dev'))
 
