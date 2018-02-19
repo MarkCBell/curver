82e024f05730a1791b314ef59d6e8f53ef6abd68
Mark Bell <mcbell@illinois.edu>
2018-02-19 22:23:21 +0000

    Updated version number.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setup(
     name='curver',
-    version='0.1.1',
+    version='0.1.2',
     description='For calculations in the curve complex',
     long_description='See http://curver.readthedocs.io for the full README, LICENCE and documentation.',
     author='Mark Bell',
