b6306807cf183dbe8b34f7fb079365b0b0359446
Mark Bell <mcbell@illinois.edu>
2018-03-09 18:02:01 +0000

    Reduced number of tests.

--- tests/test_crush.py
+++ tests/test_crush.py
@@ -17,7 +17,7 @@ class TestCrush(unittest.TestCase):
         self.assertEqual(crush, pickle.loads(pickle.dumps(crush)))
     
     @given(strategies.curves())
-    @settings(max_examples=5)
+    @settings(max_examples=3)
     def test_inverse(self, curve):
         crush = curve.crush()
         self.assertEqual(crush, ~(~crush))
