34f818fb7cf9c1bbf4045d14246181c0593798ff
Mark Bell <mark00bell@googlemail.com>
2018-04-08 22:54:13 +0100

    Fix bug in determining the vertices a MultiArc connects to.

--- curver/kernel/arc.py
+++ curver/kernel/arc.py
@@ -19,7 +19,7 @@ class MultiArc(Lamination):
     def vertices(self):
         ''' Return set of vertices that the components of this MultiArc connects to. '''
         
-        return set(vertex for vertex in self.triangulation.vertices if any(self(edge) < 0 or self.side_weight(edge) > 0 for edge in vertex))
+        return set(vertex for vertex in self.triangulation.vertices if any(self(edge) < 0 or self.side_weight(edge) < 0 for edge in vertex))
     
     def boundary(self):
         ''' Return the multicurve which is the boundary of a regular neighbourhood of this multiarc. '''
