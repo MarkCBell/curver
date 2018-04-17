afc93a0f8b4962afeaa54f3430d7bc6608218d65
Mark Bell <mark00bell@googlemail.com>
2018-04-17 22:26:34 +0100

    Reduced strategy space.

--- tests/strategies.py
+++ tests/strategies.py
@@ -18,48 +18,12 @@ TRIANGULATIONS = {
     (1, 1): '3_o',
     (1, 2): '6_WKSv',
     (1, 3): '9_q3AtQIBp',
-    (1, 4): 'c_qfj5RWuFLb802',
-    (1, 5): 'f_qLjT1kC5ZTnGyy1-H',
-    (1, 6): 'i_qLbeF0856iXdcySItPVI7O',
-    (1, 7): 'l_qLH58G-B-C5woL+MsXnS96SB0Ur2',
-    (1, 8): 'o_qLH3BJ5cZdEYqFJK8US2y-FobflUMuJXh',
-    (1, 9): 'r_qLHzvY4m6fubS+XBc6Fe3q2oydqacuUFP8knNo4',
     (2, 1): '9_qBFcQEN4',
     (2, 2): 'c_q5YHALgCWTyw',
     (2, 3): 'f_q5sG87f308pRczx4k',
-    (2, 4): 'i_q5s8zbvSopJhvzCSeTFSHs',
-    (2, 5): 'l_q5sEtshJlZcP3VxnSg50en+0fYA1',
-    (2, 6): 'o_q5sEjz8JquEV7VEKDk7V1jA2MXn78nNzc',
-    (2, 7): 'r_q5sEXh18WxhR6p+c1t3FqBE5ADBr5HuhrOHDBf3',
-    (2, 8): 'u_q5sEXJrSMGIad7plECHCF-ZDpcWLW83ZkNe4eiHHmFlD1',
-    (2, 9): 'x_q5sEXd3XbSQm+-kXyrXCVYKYFcTz6Z5jHLHM+udBWiyxoM5Zbt1',
     (3, 1): 'f_0mGaGVParMxLYnCS3',
     (3, 2): 'i_0mGG3b5sZ48hMGl+XMxQf7',
     (3, 3): 'l_0mGGHziLfC85djn-maLAMR4st0K',
-    (3, 4): 'o_0mGGHv87rZuOX6acFcu2Pxuu+DqmvfRb7',
-    (3, 5): 'r_0mGGH-frv5VxbMIDZJg+PfsvsGkD+T4V2s3Up62',
-    (3, 6): 'u_0mGGH-P8pozBmiNXbK0fZpo0ncTEiu04QdNjBn9ZJgn81',
-    (3, 7): 'x_0mGGH-3ktQlFDgcyM9fkSxjeT1cb5E-PwJDw3I-lkIOMDUAcV41',
-    (3, 8): 'A_0mGGH-3rGANCo9JE6TmZaKNI6kuZUobdsYwLbLayo8Ub3hD75-FeycbL1',
-    (3, 9): 'D_0mGGH-3XUmszRC-f0Kyqkw4UM8-vbAhPgaq-V4Gb6SjpdRrqXpP6MLNexfnVuA4',
-    (4, 1): 'l_0wtlMpgmgKA+k2ObfDEOlf+tdih1',
-    (4, 2): 'o_0wtlMFe+inn8rzo8rBgSeeSJdPKrw1OFa',
-    (4, 3): 'r_0wtlMFCguCalYSPETZQ9G4DvnpCtAr6eMZFLPR2',
-    (4, 4): 'u_0wtlMF+WbZ81Ll+SKPg5sOpjSoG6gQWjbTLQZ38oPavs1',
-    (4, 5): 'x_0wtlMF+eOhAEjOyEX96St+C4dYcwSbwozSel1VJ0du3wvIPKJk1',
-    (4, 6): 'A_0wtlMF+uWL8zHWMw5In5jPZGKqe3qImPInE7fKCNV2uUrxwodSD9H7X42',
-    (4, 7): 'D_0wtlMF+uW2PslyYLdtjcGMFYloqw8elhB75LSzd7FlORRH0vo8y8+7Bsxnr0lm5',
-    (4, 8): 'G_0wtlMF+uKiqZQ4wW85xwYGeuWoKctihKeCaJXLhpKtRsAj00sS960b23XP0t67ospXIOl',
-    (4, 9): 'J_0wtlMF+uKu-uOzqzZ+7kU2Lq4I3BEeaaVa57q2C0lhFS7iV1YBCH7IbI-L3HePt7iIGBe5WgA282',
-    (5, 1): 'r_0wUXPZwUD9uA4K-fmxT93jrye5mW+6QUPZVGQU1',
-    (5, 2): 'u_0wUXPZw7sxaMvf+HM6p0m50javFCeVyOYYsOtrBCvcH21',
-    (5, 3): 'x_0wUXPZwTnJKfEIppmxHddHc+ER8yCqQ445v0d2Q101uZCcV6u01',
-    (5, 4): 'A_0wUXPZwTTUvsjvktbsTgIgjJ7aqCDyJNKbtky0Ajvrz4SWEQ+5CjlC9F1',
-    (5, 5): 'D_0wUXPZwTHMJ9w2zmjkrNnNtK7FwyVbF-mL4UHfta7eN54N2lZekGdxwvKMt1Om4',
-    (5, 6): 'G_0wUXPZwTHYiyz+sdTFf3khZz195gt99PeEiX+MLv9mUa4O4yfOqo3b--xuvog9dP2fO9i',
-    (5, 7): 'J_0wUXPZwTH0cWeOU-yEvWsxsiJOElP-Vml7Bi2JyY24itGFBv1Z0bif-hsK1c9zL-QETRIcKg9pP1',
-    (5, 8): 'M_0wUXPZwTH0cRiVJPBRoeCnlyjorpiQI8coG+UQtINlFoP-I6sOedoZGzA8cPUK3lFgQIaV7vSIaoDajr1h',
-    (5, 9): 'P_0wUXPZwTH0sVLX2dmkM054OZAYTbfU7OhNURAF5-lmsXhmepRzsMjqrme2wP+y2oFw16Ud-04jQ7uCBbFXMocr5F3',
     }
 
 SIGNATURES = [TRIANGULATIONS[key] for key in sorted(TRIANGULATIONS)]
@@ -69,7 +33,7 @@ def triangulations(draw):
     sig = draw(st.sampled_from(SIGNATURES))
     return curver.triangulation_from_sig(sig)
 
-MCGS = [curver.load(g, p) for g in range(0, 5) for p in range(1, 5) if 6*g + 3*p - 6 >= 3]
+MCGS = [curver.load(g, p) for g, p in [(0, 3), (0, 4), (1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2)]]
 
 @st.composite
 def mcgs(draw):
