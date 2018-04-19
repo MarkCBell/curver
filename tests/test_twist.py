ac95183e4c5a9bf0fa8d12cd53c84b16fe0aa16d
Mark Bell <mark00bell@googlemail.com>
2018-04-19 22:50:06 +0100

    Added test that power of twists match their expansions.

--- tests/test_twist.py
+++ tests/test_twist.py
@@ -4,6 +4,7 @@ import hypothesis.strategies as st
 import pickle
 import unittest
 
+import curver
 import strategies
 
 class TestTwist(unittest.TestCase):
@@ -25,4 +26,17 @@ class TestTwist(unittest.TestCase):
         self.assertEqual(twist_i * twist_j, twist_j * twist_i)  # Commute.
         self.assertEqual(twist_i * twist_j, twist_ij)  # Additive.
         self.assertEqual(twist_neg_i, ~twist_i)  # Inverse.
+    
+    @given(strategies.curves(), st.integers(min_value=-10, max_value=10))
+    def test_encoding(self, curve, power):
+        twist = curve.encode_twist(power)
+        encoding = curver.kernel.Encoding([subitem for item in twist for subitem in (item.encoding**item.power if isinstance(item, curver.kernel.Twist) else [item])])
+        self.assertEqual(twist, encoding)
+
+class TestHalfTwist(unittest.TestCase):
+    @given(strategies.arcs().filter(lambda a: a.connects_distinct_vertices()), st.integers(min_value=-10, max_value=10))
+    def test_encoding(self, arc, power):
+        halftwist = arc.encode_halftwist(power)
+        encoding = curver.kernel.Encoding([subitem for item in halftwist for subitem in (item.encoding**item.power if isinstance(item, curver.kernel.HalfTwist) else [item])])
+        self.assertEqual(halftwist, encoding)
 
