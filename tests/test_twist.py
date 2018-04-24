3b6b9366c896eb067a503133be9f14767a1f9912
Mark Bell <mark00bell@googlemail.com>
2018-04-24 22:18:23 +0100

    More tests.

--- tests/test_twist.py
+++ tests/test_twist.py
@@ -30,18 +30,20 @@ class TestTwist(unittest.TestCase):
         self.assertEqual(twist_i * twist_j, twist_j * twist_i)  # Commute.
         self.assertEqual(twist_i * twist_j, twist_ij)  # Additive.
         self.assertEqual(twist_neg_i, ~twist_i)  # Inverse.
-    
-    @given(strategies.curves(), st.integers(min_value=-10, max_value=10))
-    def test_encoding(self, curve, power):
-        twist = curve.encode_twist(power)
-        encoding = curver.kernel.Encoding([subitem for item in twist for subitem in (item.encoding**item.power if isinstance(item, curver.kernel.Twist) else [item])])
-        self.assertEqual(twist, encoding)
 
 class TestHalfTwist(unittest.TestCase):
-    @given(strategies.arcs().filter(lambda a: a.connects_distinct_vertices()), st.integers(min_value=-10, max_value=10))
-    def test_encoding(self, arc, power):
-        assume(power != 0)
-        halftwist = arc.encode_halftwist(power)
-        encoding = curver.kernel.Encoding([subitem for item in halftwist for subitem in (item.encoding**item.power if isinstance(item, curver.kernel.HalfTwist) else [item])])
-        self.assertEqual(halftwist, encoding)
+    @given(strategies.arcs().filter(lambda a: a.connects_distinct_vertices()), st.integers(), st.integers())
+    @settings(max_examples=2)
+    def test_powers(self, arc, power1, power2):
+        assume(power1 != 0)
+        assume(power2 != 0)
+        assume(power1 + power2 != 0)
+        htwist_i = arc.encode_halftwist(power1)
+        htwist_j = arc.encode_halftwist(power2)
+        htwist_ij = arc.encode_halftwist(power1 + power2)
+        htwist_neg_i = arc.encode_halftwist(-power1)
+        
+        self.assertEqual(htwist_i * htwist_j, htwist_j * htwist_i)  # Commute.
+        self.assertEqual(htwist_i * htwist_j, htwist_ij)  # Additive.
+        self.assertEqual(htwist_neg_i, ~htwist_i)  # Inverse.
 
