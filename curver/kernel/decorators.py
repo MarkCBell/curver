0e61409ceb870b59fb93e225edbb68ce82bf521f
Mark Bell <mark00bell@googlemail.com>
2018-07-12 21:08:29 +0100

    Updated decorators to use decorator module.

--- curver/kernel/decorators.py
+++ curver/kernel/decorators.py
@@ -1,25 +1,23 @@
 
 ''' A module for decorators. '''
 
+from decorator import decorator
 from functools import wraps
 import inspect
 
-def memoize(function):
+@decorator
+def memoize(function, *args, **kwargs):
     ''' A decorator that memoizes a method of a class. '''
-    @wraps(function)
-    def caching(self):
-        ''' The cached version of function.
-        
-        Note that this docstring will be overwritten with functions docstring by the wraps decorator. '''
-        
-        if not hasattr(self, '_cache'):
-            self._cache = dict()
-        key = function.__name__
-        if key not in self._cache:
-            self._cache[key] = function(self)
-        return self._cache[key]
     
-    return caching
+    inputs = inspect.getcallargs(function, *args, **kwargs)
+    self = inputs['self']
+    key = (function.__name__, frozenset(inputs.items()))
+    
+    if not hasattr(self, '_cache'):
+        self._cache = dict()
+    if key not in self._cache:
+        self._cache[key] = function(self)
+    return self._cache[key]
 
 def topological_invariant(function):
     ''' Mark this function as a topological invariant.
@@ -31,22 +29,17 @@ def topological_invariant(function):
 
 def ensure(*fs):
     ''' A decorator that specifies properties that the result of a functions should have. '''
-    def wrapper(function):
+    @decorator
+    def wrapper(function, *args, **kwargs):
         ''' A decorator that checks that the result of a function has properties fs. '''
-        @wraps(function)
-        def ensurer(*args, **kwargs):
-            ''' The ensuring version of function.
-            
-            Note that this docstring will be overwritten with functions docstring by the wraps decorator. '''
-            
-            result = function(*args, **kwargs)
-            data = type('data', (), inspect.getcallargs(function, *args, **kwargs))  # pylint: disable=deprecated-method
-            data.result = result
-            
-            for f in fs:
-                assert f(data)
-            return result
         
-        return ensurer
+        result = function(*args, **kwargs)
+        data = type('data', (), inspect.getcallargs(function, *args, **kwargs))  # pylint: disable=deprecated-method
+        data.result = result
+        
+        for f in fs:
+            assert f(data)
+        return result
+    
     return wrapper
 
