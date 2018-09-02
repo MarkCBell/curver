e04aa4568275cf817ee134dddb187fd299efbc3f
Mark Bell <mark00bell@googlemail.com>
2018-09-02 10:37:51 +0100

    Simpler imports.

--- curver/kernel/__init__.py
+++ curver/kernel/__init__.py
@@ -7,56 +7,26 @@ by "Assumes that ..." meaning that:
     - If the assumptions are met then this function is guaranteed to terminate correctly.
     - If not then a curver.AssumptionError will be raised. '''
 
-from . import arc
-from . import crush
-from . import curve
-from . import curvegraph
-from . import encoding
-from . import error
-from . import homology
-from . import lamination
-from . import mappingclassgroup
-from . import moves
-from . import partition
-from . import permutation
-from . import structures
-from . import triangulation
-from . import twist
-from . import utilities
+# flake8: noqa  # Can't check this file since the imports are never used.
+from .arc import Arc, MultiArc
+from .crush import Crush, Lift
+from .curve import Curve, MultiCurve
+from .curvegraph import CurveGraph
+from .encoding import Encoding, Mapping, MappingClass
+from .error import AssumptionError, AbortError
+from .homology import HomologyClass
+from .lamination import Lamination
+from .mappingclassgroup import MappingClassGroup
+from .moves import Move, FlipGraphMove, EdgeFlip, Isometry
+from .partition import CurvePartitionGraph
+from .permutation import Permutation
+from .structures import UnionFind, StraightLineProgram
+from .triangulation import Edge, Triangle, Triangulation, norm
+from .twist import Twist, HalfTwist
+from .utilities import matrix_vector
 
 # Set up shorter names for all of the different classes.
-Edge = triangulation.Edge
-Triangle = triangulation.Triangle
-Triangulation = triangulation.Triangulation
-Encoding = encoding.Encoding
-Mapping = encoding.Mapping
-MappingClass = encoding.MappingClass
-AbortError = error.AbortError
-AssumptionError = error.AssumptionError
-MappingClassGroup = mappingclassgroup.MappingClassGroup
 MCG = MappingClassGroup  # Alias.
-HomologyClass = homology.HomologyClass
-Lamination = lamination.Lamination
-MultiCurve = curve.MultiCurve
-Curve = curve.Curve
-MultiArc = arc.MultiArc
-Arc = arc.Arc
-Move = moves.Move
-FlipGraphMove = moves.FlipGraphMove
-Isometry = moves.Isometry
-EdgeFlip = moves.EdgeFlip
-Crush = crush.Crush
-Lift = crush.Lift
-CurveGraph = curvegraph.CurveGraph
-Twist = twist.Twist
-HalfTwist = twist.HalfTwist
-CurvePartitionGraph = partition.CurvePartitionGraph
-Permutation = permutation.Permutation
-
-norm = triangulation.norm
-UnionFind = structures.UnionFind
-StraightLineProgram = structures.StraightLineProgram
-matrix_vector = utilities.matrix_vector
 
 # Functions that help with construction.
 create_triangulation = Triangulation.from_tuple
