58291b822a8a0350df2aabd5165a7f1b93da78ce
Mark Bell <mark00bell@googlemail.com>
2018-09-23 22:26:14 +0100

    More example periodic mapping classes to test, including identity.

--- tests/encoding.py
+++ tests/encoding.py
@@ -87,6 +87,16 @@ class TestMappingClass(TestMapping):
         self.assertEqual(h.is_identity(), h == h.source_triangulation.id_encoding())
     
     @given(st.data())
+    def test_identity_quotient(self, data):
+        T = data.draw(strategies.triangulations())
+        
+        h = T.id_encoding()
+        self.assertEqual(h.order(), 1)
+        
+        T_signature = [(2 - 2*g - n, 1, [(True, 0, 1) for _ in range(n)]) for (g, n) in T.surface().values()]
+        self.assertEqual(h.quotient_orbifold_signature(), T_signature)
+    
+    @given(st.data())
     @settings(max_examples=2)
     def test_order(self, data):
         h = data.draw(self._strategy())
@@ -97,6 +107,10 @@ class TestMappingClass(TestMapping):
     def test_orbifold(self, data):
         # Periodic mapping classes.
         h = data.draw(st.sampled_from([
+            curver.load(0, 6)('s_0.s_1.s_2.s_3.s_4'),
+            curver.load(0, 6)('(s_0.s_1.s_2.s_3.s_4)^2'),
+            curver.load(0, 6)('(s_0.s_1.s_2.s_3.s_4)^3'),
+            curver.load(0, 6)('s_0.s_1.S_3.S_4'),
             curver.load(1, 1)('a_0.b_0'),
             curver.load(1, 1)('a_0.b_0.a_0'),
             curver.load(2, 1)('a_0.b_0.c_0.b_1'),
