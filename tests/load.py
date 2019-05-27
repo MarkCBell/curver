70387e2555abbf05c660007db61aa463995b6045
Mark Bell <mark00bell@googlemail.com>
2019-05-27 11:36:16 +0100

    Initial tests of load.py.

---
+++ tests/load.py
@@ -0,0 +1,15 @@
+from hypothesis import given, settings, assume
+import hypothesis.strategies as st
+import unittest
+
+import strategies
+
+class TestLoad(unittest.TestCase):
+    @given(st.integers(min_value=0), st.integers(min_value=1))
+    def test_pair(self, g, n):
+        assume(2 - 2*g - n < 0)
+        S = curver.load(g, n)
+        T = S.triangulation
+        self.assertEqual(S.genus, g)
+        self.assertEqual(S.num_vertices, n)
+    
