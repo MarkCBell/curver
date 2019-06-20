d10ee0b72f6afa0f4f81b11ed973833c29423fda
Mark Bell <mark00bell@googlemail.com>
2019-06-20 22:03:35 +0100

    Smaller range of load tests.

--- tests/load.py
+++ tests/load.py
@@ -7,7 +7,7 @@ import strategies
 import curver
 
 class TestLoad(unittest.TestCase):
-    @given(st.integers(min_value=0), st.integers(min_value=1))
+    @given(st.integers(min_value=0, max_value=3), st.integers(min_value=1, max_value=5))
     def test_pair(self, g, n):
         assume(2 - 2*g - n < 0)
         S = curver.load(g, n)
