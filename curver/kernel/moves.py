1471365d4c81d6d57dec7f0932a1c6248d683def
Mark Bell <mark00bell@googlemail.com>
2019-11-23 23:39:37 +0000

    More abstractmethods for Moves.

--- curver/kernel/moves.py
+++ curver/kernel/moves.py
@@ -48,6 +48,10 @@ class Move(ABC):
         return self._inverse
     
     @abstractmethod
+    def package(self):
+        ''' Return a small amount of data such that self.source_triangulation.encode([data]) == self.encode(). '''
+    
+    @abstractmethod
     def apply_lamination(self, lamination):
         ''' Return the lamination obtained by mapping the given lamination through this move. '''
     
