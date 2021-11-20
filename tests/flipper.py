94ea0b25298313450970546ba13fb8ed2afc4acf
Mark Bell <mark00bell@googlemail.com>
2021-11-20 12:36:17 +0000

    Separated out pA method tests.

--- tests/flipper.py
+++ tests/flipper.py
@@ -2,7 +2,7 @@
 import pytest
 import unittest
 
-from hypothesis import given
+from hypothesis import given, assume
 import hypothesis.strategies as st
 
 import curver
@@ -10,19 +10,30 @@ import curver
 flipper = pytest.importorskip('flipper')  # Skip these tests if flipper is not available.
 
 class TestFlipper(unittest.TestCase):
-    @given(st.data())
-    def test_pseudo_anosov(self, data):
+    @staticmethod
+    def mapping_classes(data):
         name = data.draw(st.sampled_from(['S_1_1', 'S_1_2', 'S_2_1']))
-        S = curver.load(name)
-        SS = flipper.load(name)
-        word = ''.join(data.draw(st.lists(elements=st.sampled_from(sorted(S.mapping_classes)))))
-        g = S(word)
-        h = SS(word)
-        self.assertEqual(g.is_pseudo_anosov(), h.is_pseudo_anosov())
+        S_curver = curver.load(name)
+        S_flipper = flipper.load(name)
+        word = ''.join(data.draw(st.lists(elements=st.sampled_from(sorted(S_curver.mapping_classes)))))
+        return S_curver(word), S_flipper(word)
+    
+    @given(st.data())
+    def test_nielsen_thurston_type(self, data):
+        g, h = self.mapping_classes(data)
+        self.assertEqual(g.nielsen_thurston_type(), h.nielsen_thurston_type())
+    
+    @given(st.data())
+    def test_dilatation(self, data):
+        g, h = self.mapping_classes(data)
+        assume(g.is_pseudo_anosov())
+        self.assertEqual(g.dilatation(), h.dilatation())
+    
+    @given(st.data())
+    def test_stratum(self, data):
+        g, h = self.mapping_classes(data)
+        assume(g.is_pseudo_anosov())
         
-        if g.is_pseudo_anosov():
-            self.assertEqual(g.dilatation(), h.dilatation())
-            
-            flipper_stratum = sorted(c - 2 for c in h.stratum().values())
-            self.assertEqual(g.stratum(), flipper_stratum)
+        flipper_stratum = sorted(c - 2 for c in h.stratum().values())
+        self.assertEqual(g.stratum(), flipper_stratum)
 
