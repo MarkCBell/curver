c7c6806ee95fd252935a54435bc4e0b47f8547de
Mark Bell <mark00bell@googlemail.com>
2022-01-05 00:19:39 +0000

    Moved most setup to setup.cfg.

--- setup.py
+++ setup.py
@@ -3,41 +3,6 @@
 
 '''The setup script.'''
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-requirements = [
-    'decorator>=4.3.0',
-    'networkx>=2.0',
-    'numpy>=1.15.1',
-    'realalg>=0.3.1',
-]
-
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
-setup(
-    name='curver',
-    version='0.4.1',
-    description='For calculations in the curve complex',
-    long_description=readme(),
-    author='Mark Bell',
-    author_email='mcbell@illinois.edu',
-    url='https://github.com/MarkCBell/curver',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=requirements,
-    license='MIT License',
-    zip_safe=False,
-    keywords='curver',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        ],
-)
+setup()
