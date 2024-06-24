0e5a03faf4a1b8344306627d66eda49cab7a0db1
Mark Bell <mark00bell@googlemail.com>
2024-06-24 16:37:57 +0100

    Fix healthcheck.

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,8 @@ def pytest_collection_modifyitems(config, items):
                 item.add_marker(skip_slow)
 
 
-settings.register_profile('ci', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow,)))
-settings.register_profile('dev', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow,), max_examples=5))
-settings.register_profile('debug', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow,), max_examples=5, verbosity=Verbosity.verbose))
+settings.register_profile('ci', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow, HealthCheck.differing_executors)))
+settings.register_profile('dev', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow, HealthCheck.differing_executors), max_examples=5))
+settings.register_profile('debug', settings(deadline=None, suppress_health_check=(HealthCheck.too_slow, HealthCheck.differing_executors), max_examples=5, verbosity=Verbosity.verbose))
 settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'dev'))
 
